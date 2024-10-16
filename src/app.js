7c25a3306a6ded10c1c784f27c260bb8a2adda0a
Your Name <your.email@example.com>
2024-10-16 13:51:36 +0530

    Initialised the project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.use((req, res) => {
+    res.send("hii");
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);

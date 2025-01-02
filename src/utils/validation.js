00f6133d70fd589958f354498afd1511fe926fdb
Vansh16aug <kumarvansh16aug@gmail.com>
2025-01-02 19:45:57 +0530

    fixes

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,22 +2,31 @@ import validator from "validator";
 
 const validateSignUpData = (req) => {
   const { firstName, lastName, emailId, password, age, gender } = req.body;
+  const errors = [];
 
-  if (!firstName || !lastName || !emailId || !password || !age || !gender) {
-    throw new Error("Fields can't be empty");
+  if (!firstName) errors.push("First name can't be empty");
+  if (!lastName) errors.push("Last name can't be empty");
+  if (!emailId) errors.push("Email ID can't be empty");
+  if (!password) errors.push("Password can't be empty");
+  if (!age) errors.push("Age can't be empty");
+  if (!gender) errors.push("Gender can't be empty");
+
+  if (emailId && !validator.isEmail(emailId)) {
+    errors.push("Invalid Email format");
   }
-  if (!validator.isEmail(emailId)) {
-    throw new Error("Invalid Email format : " + emailId);
-  }
-  if (!validator.isStrongPassword(password)) {
-    throw new Error(
+
+  if (password && !validator.isStrongPassword(password)) {
+    errors.push(
       "Password must be at least 8 characters long, contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol."
     );
   }
-  if (!["male", "female", "other"].includes(gender)) {
-    throw new Error(
-      "There are only 2 genders (male & female) or choose other if you are not sure kiddo"
-    );
+
+  if (gender && !["male", "female", "other"].includes(gender)) {
+    errors.push("Invalid gender. Valid options are male, female, or other.");
+  }
+
+  if (errors.length) {
+    throw new Error(errors.join(" "));
   }
 };
 
